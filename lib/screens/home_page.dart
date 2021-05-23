9b86260a9660bb444a4cfcdb14e5578484e9c10b
syauqiamiq <syauqiamiq12@gmail.com>
2021-05-23 12:43:39 +0700

    TEST

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:simpati_bot/screens/chat_page.dart';
+import 'package:simpati_bot/screens/informationPage.dart';
 import 'package:simpati_bot/screens/sidenav_page.dart';
 import 'package:simpati_bot/utils/colorPallete.dart';
 import 'package:simpati_bot/widgets/chatButton.dart';
@@ -64,9 +66,14 @@ class _HomePageState extends State<HomePage> {
         body: SafeArea(
           child: Stack(children: <Widget>[
             Container(
+              
               color: Pallete.secondaryColor,
+              
+              
               child: Column(
+                
                 children: [
+                  
                   SizedBox(
                     height: 20,
                   ),
@@ -126,7 +133,12 @@ class _HomePageState extends State<HomePage> {
                                             fontWeight: FontWeight.w700),
                                       );
                                     }
-                                    return Text("Loading");
+                                    return Text("Loading",
+                                    style: TextStyle(
+                                            fontFamily: 'Nunito',
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w700),
+                                    );
                                   },
                                 ),
                                 StreamBuilder<DocumentSnapshot>(
@@ -158,18 +170,63 @@ class _HomePageState extends State<HomePage> {
             ),
             DraggableScrollableSheet(
                 initialChildSize: 0.7,
-                maxChildSize: 0.7,
+                maxChildSize: 0.95,
                 minChildSize: 0.7,
                 builder: (BuildContext context, myScrollController) {
-                  return ClipRRect(
+                  return Container(
+                    
+                    height: 450,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          offset: Offset(0,-3),
+                          blurRadius: 10
+                        ),
+                      ]
+                    ),
+                    margin: EdgeInsets.only(top: 40),
+                    child: ClipRRect(
                     borderRadius:
                         BorderRadius.vertical(top: Radius.circular(20)),
                     child: Container(
-                      color: Colors.white,
+                      color: Colors.white70,
                       padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
                       child: ListView(
                         controller: myScrollController,
                         children: [
+                          ModernNewsCard(
+                            newsTitle: "Kenalan Yuk!",
+                            newsContent: "Jelajahi fitur-fitur serta berbagai hal menarik lainnya disini.",
+                            newsImage: AssetImage('lib/assets/images/kenalanBanner.png'),
+                            onTab: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => InformationPage(title: "Kenalan Yuk dengan Simpati Bot dan Cara menggunakannya", newsImage: AssetImage('lib/assets/images/kenalanBanner.png'), description: "Jelajahi fitur-fitur serta berbagai hal menarik lainnya disini.\nCoba tanyakan sekarang:\n\n\t Kapan Praktikum Pbo kelas A?\n \tAtau yang lainnya"),));
+                            },
+                          ),
+                          ModernNewsCard(
+                            newsTitle: "Siap Jadi Asisten Kamu!",
+                            newsContent: "Simpati  Bot siap menjawab semua pertanyaan kamu mengenai labit, praktikum dan adminitrasi..",
+                            newsImage: AssetImage('lib/assets/images/asistenBanner.png'),
+                            onTab: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()),);
+                            },
+                          ),
+                          ModernNewsCard(
+                            newsTitle: "Kenalan Yuk!",
+                            newsContent: "Jelajahi fitur-fitur serta berbagai hal menarik lainnya disini.",
+                            newsImage: AssetImage('lib/assets/images/kenalanBanner.png'),
+                            onTab: (){},
+                          ),
+                          NewsTitle(
+                            newsTitle: "Simpati Information",
+                            newsContent: "Berbagai Informasi Penting yang harus kamu ketahui",
+                          ),
                           NewsCard(
                             newsTitle: "Teknik Informatika",
                             newsContent:
@@ -215,7 +272,10 @@ class _HomePageState extends State<HomePage> {
                         ],
                       ),
                     ),
+                  ),
                   );
+                  
+
                 }),
             Container(
               width: double.infinity,
--- lib/utils/colorPallete.dart
+++ lib/utils/colorPallete.dart
@@ -3,4 +3,6 @@ import 'package:flutter/cupertino.dart';
 class Pallete {
   static const primaryColor = Color(0xffFF9F43);
   static const secondaryColor = Color(0xffFFBE76);
+  static const bgWhiteColor = Color(0xffFAB44C);
+  static const primaryCleanColor = Color(0xffFAB44C);
 }
