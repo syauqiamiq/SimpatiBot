d57bc13cac0340200b05f9f0f4163b9d9ae9a892
syauqiamiq <syauqiamiq12@gmail.com>
2021-05-25 13:53:51 +0700

    Fix Firebase Storage

--- lib/services/storage/firebase_storage.dart
+++ lib/services/storage/firebase_storage.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:simpati_bot/models/userFromFirebase.dart';
+import 'package:path/path.dart';
 
 class StorageServices {
   static Future uploadImage(File imageFile) async {
-    // String fileName = basename(imageFile.path);
+    final String userUID = FirebaseAuth.instance.currentUser.uid;
+    String fileName = basename(imageFile.path);
     Reference ref =
-        FirebaseStorage.instance.ref('profile').child(UserDB.userUid + ".jpeg");
+        FirebaseStorage.instance.ref('profile').child(userUID + ".jpeg");
     await ref.putFile(imageFile);
     print("SUKSES UPLOAD");
     return await ref.getDownloadURL();
